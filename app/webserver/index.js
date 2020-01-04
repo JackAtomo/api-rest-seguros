bd824468f48f92cebbf0ad66df9bf31c9a6e6256
Diego <diego-cernadas-sexto@htomail.com>
2020-01-04 17:36:48 +0100

    Initial commit

---
+++ app/webserver/index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const express = require('express');
+
+const {
+    accountRouter,
+    loginRouter,
+} = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+app.use('/api', accountRouter);
+app.use('/api', loginRouter);
+
+let server = null;
+async function listen(port) {
+    if (server) {
+        return server;
+    }
+
+    try {
+        server = await app.listen(port);
+        return server;
+    } catch (e) {
+        console.error(e);
+        throw e;
+    }
+}
+
+async function close() {
+    if (server) {
+        await server.close();
+        server = null;
+    } else {
+        console.error('Can not close a non started server');
+    }
+}
+
+module.exports = {
+    listen,
+    close,
+};
