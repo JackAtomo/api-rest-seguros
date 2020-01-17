085c3cf4d0fa91037c5ae124a61e8634f8b41a77
Diego <diego-cernadas-sexto@htomail.com>
2020-01-17 21:51:36 +0100

    VAAAA
    vamos a ello

--- app/webserver/index.js
+++ app/webserver/index.js
@@ -4,7 +4,10 @@ const express = require('express');
 
 const {
     accountRouter,
+    adminRouter,
+    insuranceRouter,
     loginRouter,
+    serviceRouter,
 } = require('./routes');
 const app = express();
 
@@ -18,8 +21,10 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 app.use('/api', accountRouter);
+app.use('/api', adminRouter);
+app.use('/api', insuranceRouter);
 app.use('/api', loginRouter);
-
+app.use('/api', serviceRouter);
 
 let server = null;
 async function listen(port) {
